0ec196960df205fb2b4b2f3b2942927da0448592
Yann GUO <yann.guo@tlscontact.com>
2015-03-17 19:19:23 +0800

    Add removal of model file in uninstall action.

--- installer/actions/UninstallAction.php
+++ installer/actions/UninstallAction.php
@@ -17,19 +17,25 @@ class UninstallAction extends InstallerAction
         $migration = $this->migration;
         $warning = "ATTENTION!!!\n";
         $this->controller->stdout($warning, Console::FG_RED, Console::BOLD);
+        $filename = Yii::getAlias('@' . str_replace('\\', '/', $this->controller->modelClass)) . '.php';
 
-        $warning = "You are about to uninstall the Hermes Mailing extension. ";
-        $warning .= "This will delete table {$migration->tableName}. ";
-        $warning .= "All data will be erased, please be sure that you are aware of the consequence!\n";
+        $warning = "You are about to uninstall the Hermes Mailing extension. " . PHP_EOL;
+        $warning .= "This will drop database table {$migration->tableName}. " . PHP_EOL;
+        file_exists($filename) && ($warning .= "And delete model file {$filename} " . PHP_EOL);
+        $warning .= PHP_EOL;
+        $warning .= "All data will be erased, please be sure that you are aware of the consequence!" . PHP_EOL;
         $warning .= "Enter [yes] to continue: ";
         $this->controller->stdout($warning, Console::FG_RED);
         if (!$this->controller->confirm(null)) {
             $this->controller->userCancel();
         } else {
+            if (file_exists($filename)) {
+                $this->controller->stdout("Deleting model file: \n" . $filename . PHP_EOL);
+                unlink($filename);
+            }
             $migration->down();
-            $msg = "Uninstall complete!\n";
-            $msg .= "Table in database removed. ";
-            $msg .= "You may need to manually remove the corresponding AR model.\n";
+            $msg = "Uninstall complete!" . PHP_EOL;
+            $msg .= "Table in database removed. " . PHP_EOL;
             $this->controller->stdout($msg, Console::FG_GREEN);
             return 0;
         }
